08fab1e1b961bf3c3768a17233139806ada78840
Adam Cummins <adamrobertcummins@gmail.com>
2020-11-16 22:53:07 -0500

    added js object for projects and js validation

--- js/action.js
+++ js/action.js
@@ -1,13 +1,70 @@
-const email = document.getElementById("mail");
-const firstName = document.getElementById("firstName");
-const lastName = document.getElementById("lastName");
-const form = document.getElementById("form");
-const errorVar = document.getElementById('error');
+// Form Validation for contact box
 
-form.addEventListener('submit', (e) => {
-    
-    if (firstName.value === '' || name.value == null) {
-        alert('Please enter your name.');
-        e.preventDefault();
+function validateForm() {
+    var x = document.forms["contactForm"]["visitor_name"].value;
+    var y = document.forms["contactForm"]["visitor_lastName"].value;
+    var z = document.forms["contactForm"]["visitor_email"].value;
+    if (x == "") {
+      alert("First name must be filled out");
+      return false;
     }
-})
+    else if (y == "") {
+        alert("Last name must be filled out");
+        return false;
+      }
+    else if (z == "") {
+        alert("Email must be filled out");
+        return false;
+      }
+  }
+
+
+//   Projects (Placeholders)
+
+const projects = [
+    {
+    title: "Jimbo's Shrimp",
+    language: 'HTML, CSS, JavaScript',
+    image: 'jimbo.jpg'
+    },
+    {
+    title: 'Magic 8 Ball',
+    language: 'JavaScript, some HTML & CSS',
+    image: 'eight.jpg'
+    },
+    {
+    title: 'Application',
+    language: 'Primarily C#',
+    image: 'app.jpg'
+    },
+    {
+    title: 'Lottery Numbers Generator',
+    language: 'JavaScript, some HTML & CSS',
+    image: 'lotto.jpg'
+    },
+    {
+    title: 'Realtor Website',
+    language: 'HTML, CSS, JavaScript',
+    image: 'realtor.jpg'
+    }
+];
+
+const list = document.querySelector('#viewProject'); 
+
+let listOfProjects = '';
+
+for (let i = 0; i < 5; i++ ) {
+    let project = projects[i];
+    listOfProjects += 
+    `
+    <div class="jsProjects">
+        <h2>${project.title}</h2>
+        <h3>${project.language}</h3>
+        <div>
+          <a href="#"><img class="jsPics" src="images/${project.image}" alt="Preview of project"></a>
+        </div>
+    </div> 
+    `; 
+}
+
+list.innerHTML = listOfProjects;
